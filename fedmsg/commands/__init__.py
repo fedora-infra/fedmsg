b5bac666443d27b267492bb843762c8f65bdb043
Ralph Bean <rbean@redhat.com>
2012-03-14 22:22:11 -0700

    Moved _get_kwargs to the @command decorator.

--- fedmsg/commands/__init__.py
+++ fedmsg/commands/__init__.py
@@ -1,14 +1,21 @@
 import fedmsg.config
 
+
 class command(object):
-    def __init__(self, extra_args):
+    """ Convenience decorator for wrapping fedmsg console script commands.
+
+    Accepts a list of extra args.  See fedmsg.commands.logger for an example.
+    """
+
+    def __init__(self, extra_args=None):
         self.extra_args = extra_args or []
 
     def __call__(self, func):
 
         def wrapper():
             args = fedmsg.config.process_arguments(self.extra_args)
-            return func(args)
+            kwargs = dict(args._get_kwargs())
+            return func(**kwargs)
 
         # TODO -- this doesn't work.
         wrapper.__doc__ = func.__doc__
--- fedmsg/commands/logger.py
+++ fedmsg/commands/logger.py
@@ -4,9 +4,10 @@ import fedmsg
 import fedmsg.schema
 from fedmsg.commands import command
 
-def _log_message(args, message):
+
+def _log_message(kw, message):
     fedmsg.send_message(
-        topic='logger.%s' % args.topic,
+        topic='logger.%s' % kw['topic'],
         msg={fedmsg.schema.LOG: message},
         guess_modname=False,
     )
@@ -28,21 +29,22 @@ extra_args = [
     }),
 ]
 
+
 @command(extra_args=extra_args)
-def main(args):
+def logger(**kwargs):
     """ Emit log messages to the FI bus.
 
     If --message is not specified, this command accepts messages from stdin.
     """
 
-    kw = dict(args._get_kwargs())
-    kw['publish_endpoint'] = None  # Override default publishing behavior
-    fedmsg.init(**kw)
+    # Override default publishing behavior
+    kwargs['publish_endpoint'] = None
+    fedmsg.init(**kwargs)
 
-    if args.message:
-        _log_message(args, args.message)
+    if kwargs.get('message', None):
+        _log_message(kwargs, kwargs['message'])
     else:
         line = sys.stdin.readline()
         while line:
-            _log_message(args, line.strip())
+            _log_message(kwargs, line.strip())
             line = sys.stdin.readline()
