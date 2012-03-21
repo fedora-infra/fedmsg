c184439bbafb32be91d026d1f584d65bd351ba29
Ralph Bean <rbean@redhat.com>
2012-03-21 18:39:49 -0400

    Smarter config loading.
     - Loading config files works now.
     - Config loading now correctly fails over to each successive method.
     - Casting to lists is done where appropriate.
    
    Added:
    
     - A --print-config option for debugging.
     - A --config-filename option to specify a certain .ini file to use.

--- fedmsg/config.py
+++ fedmsg/config.py
@@ -7,9 +7,9 @@ Configuration values are determined by checking in the following order
     - Command line arguments
 
 For example, if a config value does not appear in either the config file or on
-the command line, then the built-in default is used.  If a value appears in both
-the config file and as a command line argument, then the command line value is
-used.
+the command line, then the built-in default is used.  If a value appears in
+both the config file and as a command line argument, then the command line
+value is used.
 """
 
 import argparse
@@ -17,22 +17,55 @@ import collections
 import copy
 import inspect
 import os
+import pprint
+import sys
 
 import ConfigParser
 
-# TODO -- centralize all defaults here
-defaults = collections.OrderedDict(
+defaults = dict(
     io_threads=1,
     topic_prefix="org.fedoraproject",
     post_init_sleep=0.5,
 )
 
 
-def load_config(extra_args, doc):
+def load_config(extra_args, doc, filenames=None):
+    """ Setup a config file from the following sources ordered by importance:
+
+      - defaults
+      - config file
+      - command line arguments
+
+    """
+
     config = copy.deepcopy(defaults)
-    config.update(_process_config_file())
+    config.update(_process_config_file(filenames=filenames))
     config.update(_process_arguments(extra_args, doc, config))
+
+    # If the user specified a config file on the command line, then start over
+    # but read in that file instead.
+    if not filenames and config.get('config_filename', None):
+        return load_config(extra_args, doc,
+                           filenames=[config['config_filename']])
+
+    # Do some post-load type massaging
+    for args, kwargs in extra_args:
+        # For arguments that accept multiple items (like a list of endpoints,
+        # for instance), break those items down into lists from comma separated
+        # strings.  That is, do so unless they are already lists.
+        if kwargs.get('nargs', None) is '*' and \
+           type(config[kwargs['dest']]) != list:
+            config[kwargs['dest']] = [
+                item.strip() for item in config[kwargs['dest']].split(',')
+            ]
+
+    # Just a little debug option.  :)
+    if config['print_config']:
+        pprint.pprint(config)
+        sys.exit(0)
+
     return config
+
 
 def _process_arguments(declared_args, doc, config):
     parser = argparse.ArgumentParser(description=doc)
@@ -58,18 +91,57 @@ def _process_arguments(declared_args, doc, config):
         help="Number of seconds to sleep after initializing.",
         default=config['post_init_sleep'],
     )
+    parser.add_argument(
+        '--config-filename',
+        dest='config_filename',
+        help="Config file to use.",
+        default=None,
+    )
+    parser.add_argument(
+        '--print-config',
+        dest='print_config',
+        help='Simply print out the configuration and exit.  No action taken.',
+        default=False,
+        action='store_true',
+    )
 
     for args, kwargs in declared_args:
+
+        # Replace the hard-coded extra_args default with the config file value
+        # (if it exists)
+        if all([k in kwargs for k in ['dest', 'default']]):
+            kwargs['default'] = config.get(
+                kwargs['dest'], kwargs['default'])
+
+        # Having slurped smart defaults from the config file, add the CLI arg.
         parser.add_argument(*args, **kwargs)
 
     args = parser.parse_args()
     return dict(args._get_kwargs())
 
-def _process_config_file():
-    parser = ConfigParser.ConfigParser(defaults=defaults)
-    files = parser.read(filenames=[
-        '/etc/fedmsg.ini',
-        os.path.expanduser('~/.fedmsg.ini'),
-        os.getcwd() + '/fedmsg.ini',
-    ])
-    return {}
+
+def _process_config_file(filenames=None):
+
+    filenames = filenames or []
+
+    # Validate that these files are really files
+    for fname in filenames:
+        if not os.path.isfile(fname):
+            raise ValueError("%r is not a file." % fname)
+
+    # If nothing specified, look in the default locations
+    if not filenames:
+        filenames = [
+            '/etc/fedmsg.ini',
+            os.path.expanduser('~/.fedmsg.ini'),
+            os.getcwd() + '/fedmsg.ini',
+        ]
+
+    parser = ConfigParser.SafeConfigParser()
+    files = parser.read(filenames=filenames)
+
+    config = {}
+    if parser.has_section('fedmsg'):
+        config = dict(parser.items('fedmsg'))
+
+    return config
