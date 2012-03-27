2151285c3268f9b6e2e49e4ddba594f72ed3ae79
Ralph Bean <rbean@redhat.com>
2012-03-27 15:56:30 -0400

    Starting work on 0.0.2.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import logging
 
 setup(
     name='fedmsg',
-    version='0.0.1',
+    version='0.0.2',
     description="Fedora Messaging Client API",
     long_description=long_description,
     author='Ralph Bean',
