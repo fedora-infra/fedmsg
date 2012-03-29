be1246014af21c116c0012a1d6dc760c6bde9008
Ralph Bean <rbean@redhat.com>
2012-03-29 14:07:07 -0400

    Updates for bodhi support.

--- fedmsg/schema.py
+++ fedmsg/schema.py
@@ -1,6 +1,7 @@
-USER = 'user'
-TAG = 'tag'
-LOG = 'log'
+USER = 'user'       # ?
+TAG = 'tag'         # For fedora-tagger
+LOG = 'log'         # For fedmsg-logger
+UPDATE = 'update'   # For bodhi
 
 # Used only for testing and developing.
 TEST = 'test'
