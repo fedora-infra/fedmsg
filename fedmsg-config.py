201744faddffadfd874815142689f021e1b8e65b
Ralph Bean <rbean@redhat.com>
2012-04-03 10:01:56 -0400

    Development config to be shared with bodhi, fas, etc..

--- fedmsg-config.py
+++ fedmsg-config.py
@@ -6,14 +6,22 @@ config = dict(
     # messages.  fedmsg.init(...) requires that a 'name' argument be passed
     # to it which corresponds with one of the keys in this dict.
     endpoints=dict(
+
         # A default bind endpoint.  Just here for debugging.  All
         # services should be named.
         default="tcp://*:6543",
 
         # This is the output side of the relay to which all other
         # services can listen.
-        relay_outbound="tcp://*:5432",
+        relay_outbound="tcp://*:4001",
 
+        # For other, more 'normal' services, fedmsg will try to guess the
+        # name of it's calling module to determine which endpoint definition
+        # to use.  This can be overridden by explicitly providing the name in
+        # the initial call to fedmsg.init(...).
+        bodhi="tcp://*:3001",
+        fas="tcp://*:3002",
+        fedoratagger="tcp://*:3003",
     ),
 
     # This is the address of an active->passive relay.  It is used for the
