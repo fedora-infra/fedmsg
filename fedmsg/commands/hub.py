953fb507b1cdc41a6d03322e4b89c775c801400f
Ralph Bean <rbean@redhat.com>
2012-04-03 16:38:36 -0400

    Use latest change to the moksha-hub api.

--- fedmsg/commands/hub.py
+++ fedmsg/commands/hub.py
@@ -1,9 +1,7 @@
 import fedmsg
 from fedmsg.commands import command
-from fedmsg.config import get_fedmsg_config_path
 
-extra_args = [
-]
+extra_args = []
 
 
 @command(extra_args=extra_args)
@@ -11,4 +9,4 @@ def hub(**kw):
     """ Run the fedmsg hub. """
 
     from moksha.hub.hub import main
-    main(config_path=get_fedmsg_config_path())
+    main(options=kw)
