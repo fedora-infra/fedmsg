930a5f83e3ed57a3fd7db9852d5a68668cfca799
Ralph Bean <rbean@redhat.com>
2012-04-03 11:52:21 -0400

    Added a --json-input option to fedmsg-logger.

--- fedmsg/commands/logger.py
+++ fedmsg/commands/logger.py
@@ -1,13 +1,18 @@
 import sys
 
 import fedmsg
+import fedmsg.json
 from fedmsg.commands import command
 
 
 def _log_message(kw, message):
+    msg = {'log': message}
+    if kw['json_input']:
+        msg = fedmsg.json.loads(message)
+
     fedmsg.send_message(
         topic=kw['topic'],
-        msg={'log': message},
+        msg=msg,
         modname=kw['modname'],
     )
 
@@ -15,6 +20,12 @@ extra_args = [
     (['--message'], {
         'dest': 'message',
         'help': "The message to send.",
+    }),
+    (['--json-input'], {
+        'dest': 'json_input',
+        'action': 'store_true',
+        'default': False,
+        'help': "Take each line of input as JSON.",
     }),
     (['--topic'], {
         'dest': 'topic',
