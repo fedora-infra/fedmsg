87cd5fe1e8a368f224a50e5694355700acaa7d8e
Ralph Bean <rbean@redhat.com>
2012-04-03 10:32:52 -0400

    Removed vestigial argument.

--- fedmsg/commands/status.py
+++ fedmsg/commands/status.py
@@ -4,21 +4,15 @@ from fabulous.color import red, green
 import fedmsg
 from fedmsg.commands import command
 
+
 def _colorize(success):
     if success:
         return green("  OK  ")
     else:
         return red(" FAIL ")
 
-extra_args = [
-    (['--producers'], {
-        'dest': 'endpoints',
-        'help': 'The list of producers to check',
-        'metavar': 'endpoint',
-        'nargs': '*',
-        'default': ["tcp://127.0.0.1:6543"],
-    }),
-]
+extra_args = []
+
 
 @command(extra_args=extra_args)
 def status(**kwargs):
