8f1a5878c9c94a69e022caefb01b8a852ceab7d2
Ralph Bean <rbean@redhat.com>
2012-04-04 13:52:05 -0400

    Disabled hub.send_message.

--- fedmsg/commands/hub.py
+++ fedmsg/commands/hub.py
@@ -1,26 +1,18 @@
 import fedmsg
 from fedmsg.commands import command
 
-extra_args = [
-    ([], {
-        'dest': 'name',
-        'metavar': "NAME",
-        'help': "Name of the publishing endpoint to use.",
-    }),
-]
+extra_args = []
 
 
 @command(extra_args=extra_args)
 def hub(**kw):
     """ Run the fedmsg hub. """
 
-    if kw['name'] not in kw['endpoints']:
-        raise ValueError("NAME must be one of %r" % kw['endpoints'].keys())
-
     # Rephrase the fedmsg-config.py config as moksha *.ini format.
+    # Note that the hub we kick off here cannot send any message.  You should
+    # use fedmsg.send_message(...) still for that.
     moksha_options = dict(
         zmq_enabled=True,
-        zmq_publish_endpoints=kw['endpoints'][kw['name']],
         zmq_subscribe_endpoints=','.join(kw['endpoints'].values()),
         zmq_strict=True,
     )
