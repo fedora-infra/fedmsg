a9c399c79a5bb5be05db976d72689fa32fe96c9e
Ralph Bean <rbean@redhat.com>
2012-04-04 13:33:35 -0400

    Attempt to wrap moksha-hub with fedmsg-hub.

--- fedmsg/commands/hub.py
+++ fedmsg/commands/hub.py
@@ -1,12 +1,30 @@
 import fedmsg
 from fedmsg.commands import command
 
-extra_args = []
+extra_args = [
+    ([], {
+        'dest': 'name',
+        'metavar': "NAME",
+        'help': "Name of the publishing endpoint to use.",
+    }),
+]
 
 
 @command(extra_args=extra_args)
 def hub(**kw):
     """ Run the fedmsg hub. """
 
-    from moksha.hub.hub import main
+    if kw['name'] not in kw['endpoints']:
+        raise ValueError("NAME must be one of %r" % kw['endpoints'].keys())
+
+    # Rephrase the fedmsg-config.py config as moksha *.ini format.
+    moksha_options = dict(
+        zmq_enabled=True,
+        zmq_publish_endpoints=kw['endpoints'][kw['name']],
+        zmq_subscribe_endpoints=','.join(kw['endpoints'].values()),
+        zmq_strict=True,
+    )
+    kw.update(moksha_options)
+
+    from moksha.hub import main
     main(options=kw)
