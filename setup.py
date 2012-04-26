b3056d2575b8c64e9bbaad2664110ba5b4a5b1b2
Ralph Bean <rbean@redhat.com>
2012-04-26 12:56:02 -0400

    Release for busmon+websocket options.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except Exception:
 
 setup(
     name='fedmsg',
-    version='0.1.0',
+    version='0.1.1',
     description="Fedora Messaging Client API",
     long_description=long_description,
     author='Ralph Bean',
