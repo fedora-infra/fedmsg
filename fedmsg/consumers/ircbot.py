71c5d0b590f88bec4e6ae11e1d1e200151e28b67
Ralph Bean <rbean@redhat.com>
2012-05-09 18:12:01 -0400

    Simplify IRCBot factory->protocol pattern.

--- fedmsg/consumers/ircbot.py
+++ fedmsg/consumers/ircbot.py
@@ -17,41 +17,26 @@ import logging
 log = logging.getLogger("moksha.hub")
 
 
-def make_client_class(consumer):
-    class FedMsngr(irc.IRCClient):
-        def _get_nickname(self):
-            return self.factory.nickname
-        nickname = property(_get_nickname)
+class FedMsngr(irc.IRCClient):
+    def _get_nickname(self):
+        return self.factory.nickname
+    nickname = property(_get_nickname)
 
-        def signedOn(self):
-            self.join(self.factory.channel)
-            print "Signed on as %s." % (self.nickname,)
+    def signedOn(self):
+        self.join(self.factory.channel)
+        log.info("Signed on as %s." % (self.nickname,))
 
-        def joined(self, channel):
-            print "Joined %s." % (channel,)
-            consumer.add_irc_client(self)
-
-        def privmsg(self, user, channel, msg):
-            if not user:
-                return
-            if self.nickname in msg:
-                prefix = user.split('!', 1)[0] + ": "
-            else:
-                prefix = ''
-            self.msg(self.factory.channel, prefix + "Reply!")
-
-        def consume(self, message):
-            self.msg(self.factory.channel, message)
-
-    return FedMsngr
-
+    def joined(self, channel):
+        log.info("Joined %s." % (channel,))
+        self.factory.parent_consumer.add_irc_client(self)
 
 class FedMsngrFactory(protocol.ClientFactory):
+    protocol = FedMsngr
+
     def __init__(self, channel, nickname, parent_consumer):
         self.channel = channel
         self.nickname = nickname
         self.parent_consumer = parent_consumer
-        self.protocol = make_client_class(parent_consumer)
 
     def clientConnectionLost(self, connector, reason):
         print "Lost connection (%s), reconnecting." % (reason,)
@@ -98,4 +83,4 @@ class IRCBotConsumer(Consumer):
     def consume(self, msg):
         """ Forward on messages from the bus to all IRC connections. """
         for client in self.irc_clients:
-            client.consume(fedmsg.json.dumps(msg))
+            client.msg(client.factory.channel, fedmsg.json.dumps(msg))
