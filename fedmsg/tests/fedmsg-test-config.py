d25a0f4fc9951e8121379f687c6596a40da30fd2
Ralph Bean <rbean@redhat.com>
2012-06-04 17:09:00 -0400

    Starting to stub out tests.

---
+++ fedmsg/tests/fedmsg-test-config.py
@@ -0,0 +1,78 @@
+config = dict(
+    # This is a dict of possible addresses from which fedmsg can send
+    # messages.  fedmsg.init(...) requires that a 'name' argument be passed
+    # to it which corresponds with one of the keys in this dict.
+    endpoints={
+        # This is the output side of the relay to which all other
+        # services can listen.
+        "relay_outbound": ["tcp://*:4001"],
+
+        # For other, more 'normal' services, fedmsg will try to guess the
+        # name of it's calling module to determine which endpoint definition
+        # to use.  This can be overridden by explicitly providing the name in
+        # the initial call to fedmsg.init(...).
+        "bodhi.marat": ["tcp://*:3001"],
+        "fas.marat": ["tcp://*:3002"],
+        "fedoratagger.marat": ["tcp://*:3003"],
+        "mediawiki.marat": ["tcp://*:3004"],
+        "pkgdb.marat": ["tcp://*:3005"],
+
+        "busmon.marat": ["tcp://*:3006"],
+    },
+
+    # This is the address of an active->passive relay.  It is used for the
+    # fedmsg-logger command which requires another service with a stable
+    # listening address for it to send messages to.
+    # It is also used by the git-hook, for the same reason.
+    # It is also used by the mediawiki php plugin which, due to the oddities of
+    # php, can't maintain a single passive-bind endpoint of it's own.
+    relay_inbound="tcp://127.0.0.1:2003",
+
+
+    # Set this to dev if you're hacking on fedmsg or an app.
+    # Set to stg or prod if running in the Fedora Infrastructure
+    environment="dev",
+
+    # Default is 0
+    high_water_mark=0,
+
+    io_threads=1,
+
+    # Options for the fedmsg-irc service.
+    irc=[
+        dict(
+            network='irc.freenode.net',
+            port=6667,
+            nickname='fedmsg-bot',
+            channel='test-fedmsg',
+            make_pretty=True,
+            filters=dict(
+                topic=[],
+                body=['lub-dub'],
+            )
+        ),
+        dict(
+            network='irc.freenode.net',
+            port=6667,
+            nickname='fedmsg-bot',
+            channel='test-fedmsg2',
+            make_pretty=True,
+            filters=dict(
+                topic=[],
+                body=['lub-dub'],
+            ),
+        ),
+    ],
+
+
+    ## For the fedmsg-hub and fedmsg-relay. ##
+
+    # We almost always want the fedmsg-hub to be sending messages with zmq as
+    # opposed to amqp or stomp.
+    zmq_enabled=True,
+
+    # When subscribing to messages, we want to allow splats ('*') so we tell the
+    # hub to not be strict when comparing messages topics to subscription
+    # topics.
+    zmq_strict=False,
+)
---
+++ fedmsg/tests/test_hub.py
@@ -0,0 +1,81 @@
+# This file is part of Moksha.
+# Copyright (C) 2008-2010  Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test Moksha's Hub """
+
+import moksha
+import unittest
+
+import os
+
+from time import sleep, time
+from uuid import uuid4
+
+from unittest import TestCase
+
+from moksha.hub.reactor import reactor as _reactor
+from moksha.hub.hub import MokshaHub
+from nose.tools import eq_, assert_true, assert_false
+
+import fedmsg.config
+
+
+# Some constants used throughout the hub tests
+sleep_duration = 0.25
+secret = "secret_message"
+
+
+def simulate_reactor(duration=sleep_duration):
+    """ Simulate running the reactor for `duration` milliseconds """
+    global _reactor
+    start = time()
+    while time() - start < duration:
+        _reactor.doPoll(0.0001)
+        _reactor.runUntilCurrent()
+
+
+class TestHub(TestCase):
+
+    def setUp(self):
+        here = os.path.sep.join(__file__.split(os.path.sep)[:-1])
+        test_config = os.path.sep.join([here, 'fedmsg-test-config.py'])
+
+        config = fedmsg.config.load_config(
+            [],
+            "awesome",
+            filenames=[
+                test_config,
+            ],
+        )
+
+        # TODO -- this appears everywhere and should be encapsulated in a func
+        # Massage the fedmsg config into the moksha config.
+        config['zmq_subscribe_endpoints'] = ','.join(
+            ','.join(bunch) for bunch in config['endpoints'].values()
+        )
+
+        self.hub = MokshaHub(config=config)
+
+    def tearDown(self):
+        self.hub.close()
+
+    def test_run_hub_get_heartbeat(self):
+        """ Start the heartbeat producer and ensure it emits a message. """
+        self.skipTest("Not implemented.")
+
+    def test_send_recv(self):
+        """ Send a message and receive it. """
+        self.skipTest("Not implemented.")
+
