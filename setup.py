cb269432f9e49226475b326e4cc39f99d5afe74a
Ralph Bean <rbean@redhat.com>
2012-06-10 01:11:12 -0400

    Version bump for systemctl+daemons.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if sys.version_info[0] == 2 and sys.version_info[1] <= 6:
 
 setup(
     name='fedmsg',
-    version='0.1.8',
+    version='0.2.0',
     description="Fedora Messaging Client API",
     long_description=long_description,
     author='Ralph Bean',
