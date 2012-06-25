44c4a3662f50a8090b81cf53916bf3d3cf3410ca
Ralph Bean <rbean@redhat.com>
2012-06-25 15:33:10 -0400

    Reduce json encoding overhead.  Fixes #42.

--- fedmsg/json.py
+++ fedmsg/json.py
@@ -22,18 +22,10 @@ class FedMsgEncoder(simplejson.encoder.JSONEncoder):
             return time.mktime(obj.timetuple())
         return super(FedMsgEncoder, self).default(obj)
 
-encoder = FedMsgEncoder()
-
-
-def dumps(d):
-    """ Encode a dict as JSON.
-
-    Ensure that the keys are ordered so that messages can be signed
-    consistently.  See https://github.com/ralphbean/fedmsg/issues/42
-    """
-    # TODO - Look into using the sort_keys argument to simplejson.  It should
-    # introduce less overhead than all this potential dict-copying.
-    return encoder.encode(OrderedDict(sorted(d.items())))
+# Ensure that the keys are ordered so that messages can be signed
+# consistently.  See https://github.com/ralphbean/fedmsg/issues/42
+encoder = FedMsgEncoder(sort_keys=True)
+dumps = encoder.encode
 
 pretty_encoder = FedMsgEncoder(indent=2)
 pretty_dumps = pretty_encoder.encode
