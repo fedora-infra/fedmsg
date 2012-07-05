8d8a921624fb5c2a5c3812dca8cf5614fe605e1a
Ralph Bean <rbean@redhat.com>
2012-07-05 13:10:36 -0400

    Version bump for crypto and new consumer API.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ if sys.version_info[0] == 2 and sys.version_info[1] <= 6:
 
 setup(
     name='fedmsg',
-    version='0.2.1',
+    version='0.2.2',
     description="Fedora Messaging Client API",
     long_description=long_description,
     author='Ralph Bean',
