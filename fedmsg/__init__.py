afa4a9ae22319b84fc5396d5e72cc971d4807dc0
Ralph Bean <rbean@redhat.com>
2012-08-09 09:57:54 -0400

    First dumb stab at thread safety.

--- fedmsg/__init__.py
+++ fedmsg/__init__.py
@@ -19,6 +19,8 @@
 #
 """ Fedora Messaging Client API """
 
+import threading
+
 import fedmsg.core
 import fedmsg.config
 
@@ -29,13 +31,15 @@ __all__ = [
     'subscribe',
 ]
 
-__context = None
+
+__local = threading.local()
+__local.__context = None
 
 
 def init(**kw):
 
-    global __context
-    if __context:
+    global __local
+    if __local.__context:
         raise ValueError("fedmsg already initialized")
 
     # Read config from CLI args and a config file
@@ -44,18 +48,18 @@ def init(**kw):
     # Override the defaults with whatever the user explicitly passes in.
     config.update(kw)
 
-    __context = fedmsg.core.FedMsgContext(**config)
-    return __context
+    __local.__context = fedmsg.core.FedMsgContext(**config)
+    return __local.__context
 
 
 def API_function(func):
 
     def _wrapper(*args, **kw):
 
-        global __context
-        if not __context:
+        global __local
+        if not __local.__context:
             init(**kw)
-            assert(__context)
+            assert(__local.__context)
 
         return func(*args, **kw)
     return _wrapper
@@ -86,7 +90,7 @@ def publish(topic=None, msg=None, **kw):
 
     """
 
-    return __context.publish(topic, msg, **kw)
+    return __local.__context.publish(topic, msg, **kw)
 
 # This is old-school, and deprecated.
 send_message = publish
@@ -101,7 +105,7 @@ def subscribe(topic, callback, **kw):
      - 'org.fedorahosted.' is prepended to the topic.
     """
 
-    return __context.subscribe(topic, callback)
+    return __local.__context.subscribe(topic, callback)
 
 
 @API_function
@@ -111,4 +115,4 @@ def have_pulses(endpoints, **kw):
     are emitting detectable heartbeats.
     """
 
-    return __context.have_pulses(endpoints)
+    return __local.__context.have_pulses(endpoints)
