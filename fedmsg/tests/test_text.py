e5ceb294a20812f103c8b24ae6c015ff4cbae212
Luke Macken <lmacken@redhat.com>
2012-08-12 23:12:43 -0400

    Improve the bodhi request messages

--- fedmsg/tests/test_text.py
+++ fedmsg/tests/test_text.py
@@ -292,12 +292,13 @@ class TestBodhiMashTaskSyncWait(Base):
 
 class TestBodhiRequestUnpush(Base):
     expected_title = "bodhi.update.request.unpush (unsigned)"
-    expected_subti = "foo requested unpush"
+    expected_subti = "lmacken unpushed foo"
     msg = {
         'topic': "org.fedoraproject.dev.bodhi.update.request.unpush",
         'msg': {
             'update': {
                 'title': 'foo',
+                'submitter': 'lmacken',
             },
         },
     }
@@ -305,12 +306,13 @@ class TestBodhiRequestUnpush(Base):
 
 class TestBodhiRequestObsolete(Base):
     expected_title = "bodhi.update.request.obsolete (unsigned)"
-    expected_subti = "foo requested obsolete"
+    expected_subti = "lmacken obsoleted foo"
     msg = {
         'topic': "org.fedoraproject.dev.bodhi.update.request.obsolete",
         'msg': {
             'update': {
                 'title': 'foo',
+                'submitter': 'lmacken',
             },
         },
     }
@@ -318,12 +320,13 @@ class TestBodhiRequestObsolete(Base):
 
 class TestBodhiRequestStable(Base):
     expected_title = "bodhi.update.request.stable (unsigned)"
-    expected_subti = "foo requested stable"
+    expected_subti = "lmacken submitted foo to stable"
     msg = {
         'topic': "org.fedoraproject.dev.bodhi.update.request.stable",
         'msg': {
             'update': {
                 'title': 'foo',
+                'submitter': 'lmacken',
             },
         },
     }
@@ -331,12 +334,13 @@ class TestBodhiRequestStable(Base):
 
 class TestBodhiRequestRevoke(Base):
     expected_title = "bodhi.update.request.revoke (unsigned)"
-    expected_subti = "foo requested revoke"
+    expected_subti = "lmacken revoked foo"
     msg = {
         'topic': "org.fedoraproject.dev.bodhi.update.request.revoke",
         'msg': {
             'update': {
                 'title': 'foo',
+                'submitter': 'lmacken',
             },
         },
     }
@@ -344,12 +348,13 @@ class TestBodhiRequestRevoke(Base):
 
 class TestBodhiRequestTesting(Base):
     expected_title = "bodhi.update.request.testing (unsigned)"
-    expected_subti = "foo requested testing"
+    expected_subti = "lmacken submitted foo to testing"
     msg = {
         'topic': "org.fedoraproject.dev.bodhi.update.request.testing",
         'msg': {
             'update': {
                 'title': 'foo',
+                'submitter': 'lmacken',
             },
         },
     }
--- fedmsg/text/bodhi.py
+++ fedmsg/text/bodhi.py
@@ -52,10 +52,18 @@ class BodhiProcessor(BaseProcessor):
             )
             return tmpl.format(author=author, package=package, status=status)
         elif 'bodhi.update.request' in msg['topic']:
-            action = msg['topic'].split('.')[-1]
+            status = msg['topic'].split('.')[-1]
+            author = msg['msg']['update']['submitter']
             title = msg['msg']['update']['title']
-            tmpl = self._("{title} requested {action}")
-            return tmpl.format(action=action, title=title)
+            if status in ('unpush', 'obsolete', 'revoke'):
+                # make our status past-tense
+                status = status + (status[-1] == 'e' and 'd' or 'ed')
+                tmpl = self._("{author} {status} {title}").format(
+                        author=author, status=status, title=title)
+            else:
+                tmpl = self._("{author} submitted {title} to {status}").format(
+                        author=author, status=status, title=title)
+            return tmpl
         elif 'bodhi.mashtask.mashing' in msg['topic']:
             repo = msg['msg']['repo']
             tmpl = self._("bodhi masher is mashing {repo}")
