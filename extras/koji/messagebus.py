700528599c954861ce7de52b08cdf55c54bdecb8
Ralph Bean <rbean@redhat.com>
2012-08-13 01:14:20 -0400

    Convert AMQP plugin to fedmsg API.

--- extras/koji/messagebus.py
+++ extras/koji/messagebus.py
@@ -1,85 +1,24 @@
-# Koji callback for sending notifications about events to a messagebus (amqp broker)
-# Copyright (c) 2009 Red Hat, Inc.
+# Koji callback for sending notifications about events to the fedmsg messagebus
+# Copyright (c) 2009-2012 Red Hat, Inc.
 #
 # Authors:
 #     Mike Bonnet <mikeb@redhat.com>
+#     Ralph Bean <rbean@redhat.com>
 
 from koji.plugin import callbacks, callback, ignore_error
-import ConfigParser
-import logging
-import qpid.messaging
-import os
-import krbV
 
 MAX_KEY_LENGTH = 255
-CONFIG_FILE = '/etc/koji-hub/plugins/messagebus.conf'
 
-config = None
-session = None
-target = None
-
-def get_sender():
-    global config, session, target
-    if session and target:
-        try:
-            return session.sender(target)
-        except:
-            logging.getLogger('koji.plugin.messagebus').warning('Error getting session, will retry', exc_info=True)
-            session = None
-            target = None
-
-    config = ConfigParser.SafeConfigParser()
-    config.read(CONFIG_FILE)
-
-    if config.getboolean('broker', 'ssl'):
-        url = 'amqps://'
-    else:
-        url = 'amqp://'
-    auth = config.get('broker', 'auth')
-    if auth == 'PLAIN':
-        url += config.get('broker', 'username') + '/'
-        url += config.get('broker', 'password') + '@'
-    elif auth == 'GSSAPI':
-        ccname = 'MEMORY:messagebus'
-        os.environ['KRB5CCNAME'] = ccname
-        ctx = krbV.default_context()
-        ccache = krbV.CCache(name=ccname, context=ctx)
-        cprinc = krbV.Principal(name=config.get('broker', 'principal'), context=ctx)
-        ccache.init(principal=cprinc)
-        keytab = krbV.Keytab(name='FILE:' + config.get('broker', 'keytab'), context=ctx)
-        ccache.init_creds_keytab(principal=cprinc, keytab=keytab)
-    else:
-        raise koji.PluginError, 'unsupported auth type: %s' % auth
-
-    url += config.get('broker', 'host') + ':'
-    url += config.get('broker', 'port')
-
-    conn = qpid.messaging.Connection.establish(url,
-                                               sasl_mechanisms=config.get('broker', 'auth'),
-                                               heartbeat=60)
-    sess = conn.session()
-    tgt = """%s;
-             { create: sender,
-               assert: always,
-               node: { type: topic,
-                       durable: %s,
-                       x-declare: { exchange: "%s",
-                                    type: %s } } }""" % \
-    (config.get('exchange', 'name'), config.getboolean('exchange', 'durable'),
-     config.get('exchange', 'name'), config.get('exchange', 'type'))
-    sender = sess.sender(tgt)
-    session = sess
-    target = tgt
-
-    return sender
 
 def _token_append(tokenlist, val):
-    # Replace any periods with underscores so we have a deterministic number of tokens
+    # Replace any periods with underscores so we have a
+    # deterministic number of tokens
     val = val.replace('.', '_')
     tokenlist.append(val)
 
+
 def get_message_subject(msgtype, *args, **kws):
-    key = [config.get('topic', 'prefix'), msgtype]
+    key = [msgtype]
 
     if msgtype == 'PackageListChange':
         _token_append(key, kws['tag']['name'])
@@ -106,6 +45,7 @@ def get_message_subject(msgtype, *args, **kws):
     key = '.'.join(key)
     key = key[:MAX_KEY_LENGTH]
     return key
+
 
 def get_message_headers(msgtype, *args, **kws):
     headers = {'type': msgtype}
@@ -142,11 +82,10 @@ def get_message_headers(msgtype, *args, **kws):
 
     return headers
 
+
 @callback(*[c for c in callbacks.keys() if c.startswith('post')])
 @ignore_error
 def send_message(cbtype, *args, **kws):
-    global config
-    sender = get_sender()
     if cbtype.startswith('post'):
         msgtype = cbtype[4:]
     else:
@@ -156,16 +95,8 @@ def send_message(cbtype, *args, **kws):
     if args:
         data['args'] = list(args)
 
-    exchange_type = config.get('exchange', 'type')
-    if exchange_type == 'topic':
-        subject = get_message_subject(msgtype, *args, **kws)
-        message = qpid.messaging.Message(subject=subject, content=data)
-    elif exchange_type == 'headers':
-        headers = get_message_headers(msgtype, *args, **kws)
-        message = qpid.messaging.Message(properties=headers, content=data)
-    else:
-        raise koji.PluginError, 'unsupported exchange type: %s' % exchange_type
-
-    sender.send(message, sync=False)
-    sender.sync(timeout=60)
-    sender.close()
+    fedmsg.publish(
+        topic=get_message_subject(msgtype, *args, **kws),
+        msg=data,
+        modname='koji',
+    )
