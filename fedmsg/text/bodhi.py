a7f9d32d410ad16f98d18bd58248dae337905b81
Ralph Bean <rbean@redhat.com>
2012-08-21 13:56:58 -0400

    Added TODO note about using new bodhi fields in the future.

--- fedmsg/text/bodhi.py
+++ fedmsg/text/bodhi.py
@@ -65,6 +65,9 @@ class BodhiProcessor(BaseProcessor):
             return tmpl.format(author=author, package=package, status=status)
         elif 'bodhi.update.request' in msg['topic']:
             status = msg['topic'].split('.')[-1]
+            # TODO -- use msg['msg']['agent'] which should be available once
+            # bodhi-0.9.3 is deployed.  The 'submitter' might not actually be
+            # the one who 'unpushes' an update, for instance.
             author = msg['msg']['update']['submitter']
             title = msg['msg']['update']['title']
             if status in ('unpush', 'obsolete', 'revoke'):
