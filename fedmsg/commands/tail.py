4dc84ec2a9f8321963bd0ebad52e74d03d65c0df
Ralph Bean <rbean@redhat.com>
2012-08-21 14:02:25 -0400

    Removed no-longer-relevant comments.

--- fedmsg/commands/tail.py
+++ fedmsg/commands/tail.py
@@ -102,8 +102,6 @@ def tail(**kw):
     # prints out each message it consumes.  That seems like overkill, so we're
     # just going to directly access the endpoints ourself.
 
-    # TODO -- colors?
-    # TODO -- tabular layout?
     for name, ep, topic, message \
             in fedmsg.__local.__context._tail_messages(**kw):
         if exclusive_regexp.search(topic):
