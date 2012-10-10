eaf263f12582b74abd45b3dd7c14e91eadc4f9b8
Ralph Bean <rbean@redhat.com>
2012-10-10 15:08:32 -0400

    Use bash correctly.

--- extras/stress/launcher.sh
+++ extras/stress/launcher.sh
@@ -3,4 +3,4 @@
 # Just an intermediary script to make invocation from xargs easier.
 
 echo "SSHing to $1"
-ssh -tt $1 "source ~/.virtualenvs/fedmsg/bin/activate; ./mass-sub.py"
+ssh -tt $1 "bash -c 'source ~/fedmsg/bin/activate; ./mass-sub.py'"
