86119bfbd62e059a61ddf4f38cd842094a273e50
Ralph Bean <rbean@redhat.com>
2012-12-03 14:27:23 -0500

    Testing fedmsg-logger --json-input.

--- fedmsg/tests/test_commands.py
+++ fedmsg/tests/test_commands.py
@@ -50,3 +50,30 @@ class TestCommands(unittest.TestCase):
                         command.execute()
 
         eq_(msgs, [{'log': test_input}])
+
+    @patch("sys.argv", new_callable=lambda: ["fedmsg-logger", "--json-input"])
+    @patch("sys.stdout", new_callable=six.StringIO)
+    def test_logger_json(self, stdout, argv):
+
+        test_input_dict = {"hello": "world"}
+        test_input = json.dumps(test_input_dict)
+
+        if six.PY3:
+            stdin = lambda: six.StringIO(test_input)
+        else:
+            stdin = lambda: six.StringIO(test_input.encode('utf-8'))
+
+        msgs = []
+
+        def mock_publish(context, topic=None, msg=None, modname=None):
+            msgs.append(msg)
+
+        config = {}
+        with patch("fedmsg.__local", self.local):
+            with patch("fedmsg.config.__cache", config):
+                with patch("fedmsg.core.FedMsgContext.publish", mock_publish):
+                    with patch("sys.stdin", new_callable=stdin):
+                        command = fedmsg.commands.logger.LoggerCommand()
+                        command.execute()
+
+        eq_(msgs, [test_input_dict])
