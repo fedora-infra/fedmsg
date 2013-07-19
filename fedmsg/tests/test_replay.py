4358d995963fa31198dcf99e9b7bc796e77e754a
Simon Chopin <chopin.simon@gmail.com>
2013-07-19 16:37:40 +0200

    test_replay: Add a license and reorganise the import statements

--- fedmsg/tests/test_replay.py
+++ fedmsg/tests/test_replay.py
@@ -1,20 +1,41 @@
-import unittest
+# This file is part of fedmsg.
+# Copyright (C) 2013 Simon Chopin <chopin.simon@gmail.com
+#
+# fedmsg is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# fedmsg is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with fedmsg; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Authors: Simon Chopin <chopin.simon@gmail.com>
+#
+''' Tests for fedmsg.replay '''
 
-from mock import Mock, call
+import unittest
 from nose.tools import assert_dict_equal, raises
+from mock import Mock, call
+
 import json
 import time
 from datetime import datetime
 import zmq
 import socket
 
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from common import load_config
 
 from fedmsg.replay import ReplayContext
-from fedmsg.replay.sqlstore import SqlStore, SqlMessage
 
-from common import load_config
+from fedmsg.replay.sqlstore import SqlStore, SqlMessage
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 hostname = socket.gethostname()
 local_name = 'unittest.{}'.format(hostname)
