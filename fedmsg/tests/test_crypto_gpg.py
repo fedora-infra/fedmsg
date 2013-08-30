983d87dbf5dd2f2643f452ef9de990c54cfcf0c7
Ralph Bean <rbean@redhat.com>
2013-08-30 13:24:46 -0400

    Skip certain gpg tests on travis since their environment is weird.
    This is an addendum to pull request #180.

--- fedmsg/tests/test_crypto_gpg.py
+++ fedmsg/tests/test_crypto_gpg.py
@@ -18,8 +18,11 @@
 # Authors:  Ralph Bean <rbean@redhat.com>
 #
 import os
+import functools
 
 from nose.tools import raises
+from nose.exc import SkipTest
+import nose.tools.nontrivial
 import unittest
 
 import fedmsg.crypto.gpg
@@ -31,6 +34,17 @@ data_dir = SEP.join((here, 'test_certs', 'gpg'))
 keyrings = []
 clear_data_path = os.path.join(data_dir, "test_data")
 secret_fp = 'FBDA 92E4 338D FFD9 EB83  F8F6 3FBD B725 DA19 B4EC'
+
+
+def skip_on_travis(fn):
+    """ A decorator that just skips some tests on travis-ci.org """
+    @functools.wraps(fn)
+    def newfunc(self, *args, **kw):
+        if os.environ.get('TRAVIS', None):
+            raise SkipTest("gpg permissions are weird on travis-ci.org")
+        return fn(self, *args, **kw)
+    return nose.tools.nontrivial.make_decorator(fn)(newfunc)
+
 
 class TestGpg(unittest.TestCase):
     def setUp(self):
@@ -46,15 +60,18 @@ class TestGpg(unittest.TestCase):
         signature_path = os.path.join(data_dir, "corrupt.sig")
         self.ctx.verify_from_file(clear_data_path, sig_path=signature_path)
 
+    @skip_on_travis
     def test_sign_cleartext(self):
         test_data = 'I can haz a signature?'
         signed_text = self.ctx.clearsign(test_data, fingerprint=secret_fp)
         self.ctx.verify(signed_text)
 
+    @skip_on_travis
     def test_sign_detached(self):
         test_data = 'I can haz a signature?'
         signature = self.ctx.sign(test_data, fingerprint=secret_fp)
         self.ctx.verify(test_data, signature)
+
 
 import fedmsg.crypto
 class TestCryptoGPG(unittest.TestCase):
@@ -68,12 +85,14 @@ class TestCryptoGPG(unittest.TestCase):
     def tearDown(self):
         self.config = None
 
+    @skip_on_travis
     def test_full_circle(self):
         """ Try to sign and validate a message. """
         message = dict(msg='awesome')
         signed = fedmsg.crypto.sign(message, **self.config)
         assert fedmsg.crypto.validate(signed, **self.config)
 
+    @skip_on_travis
     def test_failed_validation(self):
         message = dict(msg='awesome')
         signed = fedmsg.crypto.sign(message, **self.config)
