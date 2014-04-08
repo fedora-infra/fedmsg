4fe54804a24112c9d778202c8387ff3688b60c4e
Luis Bazan <lbazan@fedoraproject.org>
2014-04-08 11:46:56 -0500

    Copyright (C) 2012 - 2014 Red Hat, Inc.

--- fedmsg/tests/test_meta.py
+++ fedmsg/tests/test_meta.py
@@ -1,5 +1,5 @@
 # This file is part of fedmsg.
-# Copyright (C) 2012 Red Hat, Inc.
+# Copyright (C) 2012 - 2014 Red Hat, Inc.
 #
 # fedmsg is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
