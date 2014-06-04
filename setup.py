1126e45d5aa8bf2346615326671dc00c62be3db4
Till Maas <opensource@till.name>
2014-06-04 20:19:43 +0200

    setup.py: Fix PEP8 violation

--- setup.py
+++ setup.py
@@ -121,7 +121,8 @@ setup(
     ],
     include_package_data=True,
     zip_safe=False,
-    scripts=[ # This is separate from the other console scripts just for efficiency's
+    scripts=[
+        # This is separate from the other console scripts just for efficiency's
         # sake.  It gets called over and over and over again by our mediawiki
         # plugin/mod_php.  By making it *not* a setuptools console_script it
         # does a lot less IO work to stand up.
