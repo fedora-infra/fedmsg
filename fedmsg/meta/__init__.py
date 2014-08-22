60eff3f16a6675d4a9b9e04f8160d6d7e335d4ac
Ralph Bean <rbean@redhat.com>
2014-08-22 11:37:38 -0400

    Be more explicit with warnings from processors.
    Should have done this a long time ago.

--- fedmsg/meta/__init__.py
+++ fedmsg/meta/__init__.py
@@ -100,7 +100,7 @@ def make_processors(**config):
             processors.append(processor.load()(_, **config))
         except Exception as e:
             log.warn("Failed to load %r processor." % processor.name)
-            log.warn(str(e))
+            log.exception(e)
 
     # This should always be last
     processors.append(DefaultProcessor(_, **config))
