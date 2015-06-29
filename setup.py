07e8e11ef7e787628c830d28d72ca08f1f173ca5
Ralph Bean <rbean@redhat.com>
2015-06-29 16:08:46 -0400

    0.14.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with fedmsg; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110.14.0 USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110.14.1 USA
 #
 # Authors:  Ralph Bean <rbean@redhat.com>
 #
@@ -99,7 +99,7 @@ if sys.version_info[0] == 2:
 
 setup(
     name='fedmsg',
-    version='0.14.0',
+    version='0.14.1',
     description="Fedora Messaging Client API",
     long_description=long_description,
     author='Ralph Bean',
