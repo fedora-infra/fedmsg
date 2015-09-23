a89d87531ce2c698ed7b5d0508c84bc779779d62
Ralph Bean <rbean@redhat.com>
2015-09-23 14:51:23 -0400

    0.16.1

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ if sys.version_info[0] == 2:
 
 setup(
     name='fedmsg',
-    version='0.16.0',
+    version='0.16.1',
     description="Fedora Messaging Client API",
     long_description=long_description,
     author='Ralph Bean',
