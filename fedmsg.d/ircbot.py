b637e253a5f8597f316f9474ceae366c32e401c1
Elan Ruusamäe <glen@delfi.ee>
2016-03-19 15:00:23 +0200

    Update ircbot.py
    add link to available irc bot colors

--- fedmsg.d/ircbot.py
+++ fedmsg.d/ircbot.py
@@ -36,6 +36,8 @@ config = dict(
             ),
         ),
     ],
+    # the available colors can be looked up from here:
+    # https://github.com/fedora-infra/fedmsg/blob/0.16.4/fedmsg/consumers/ircbot.py#L48-L65
     irc_color_lookup={
         "fas": "light blue",
         "bodhi": "green",
