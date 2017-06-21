b0720831b3c1f329b44fbfc4b860dac6bfb3b1d7
Ralph Bean <rbean@redhat.com>
2017-06-21 17:53:35 -0400

    Make the ircbot url shortener configurable.
    Someone can provide something like this in their configuration to make link
    shortening work in... some other way::
    
    ```python
    config = dict(
        irc=[dict(
                make_short=lambda url: "http://awesome",
        )],
    )
    ```

--- fedmsg/consumers/ircbot.py
+++ fedmsg/consumers/ircbot.py
@@ -66,6 +66,12 @@ mirc_colors = {
 }
 
 
+def _default_link_shortener(url):
+    dagd = 'http://da.gd/s'
+    resp = requests.get(dagd, params=dict(url=url))
+    return resp.text.strip()
+
+
 def ircprettify(title, subtitle, link="", config=None):
     def markup(s, color):
         return "\x03%i%s\x03" % (mirc_colors[color], s)
@@ -307,9 +313,10 @@ class IRCBotConsumer(FedmsgConsumer):
                     link = fedmsg.meta.msg2link(msg, **self.hub.config)
 
                 if link and short:
-                    dagd = 'http://da.gd/s'
-                    resp = requests.get(dagd, params=dict(url=link))
-                    link = resp.text.strip()
+                    if callable(short):
+                        link = short(link)
+                    else:
+                        link = _default_link_shortener(link)
 
                 return ircprettify(
                     title=title,
