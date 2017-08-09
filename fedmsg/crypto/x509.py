fedora-infra/fedmsg

commit f0553d285e586dc48f1a244b81f1517551d469ee

minor refactors/pep8tification (#414)
* minor changes/pep8tification

fedmsg/crypto/x509.py
@@ -110,9 +110,9 @@ def validate(message, ssldir=None, **config):
 
     # Some sanity checking
     for field in ['signature', 'certificate']:
-        if not field in message:
+        if field not in message:
             return fail("No %r field found." % field)
-        if not isinstance(message[field], basestring):
+        if not isinstance(message[field], str):
             return fail("msg[%r] is not a string" % field)
 
     # Peal off the auth datums
@@ -150,7 +150,7 @@ def validate(message, ssldir=None, **config):
 
     # FIXME -- We need to check that the CRL is signed by our own CA.
     # See https://bugzilla.osafoundation.org/show_bug.cgi?id=12954#c2
-    #if not ctx.validate_certificate(crl):
+    # if not ctx.validate_certificate(crl):
     #    return fail("X509 CRL is not valid.")
 
     # FIXME -- we check the CRL, but by doing string comparison ourselves.
@@ -263,11 +263,11 @@ def _load_remote_cert(location, cache, cache_expiry, tries=0, **config):
                 log.warn("Could not access %r.  Trying again." % location)
                 time.sleep(1)  # Take a nap to see if the network settles down.
                 return _load_remote_cert(
-                    location, cache, cache_expiry, tries+1, **config)
+                    location, cache, cache_expiry, tries + 1, **config)
             else:
                 log.error("Could not access %r" % location)
                 raise
-        except IOError as e:
+        except IOError:
             # If we couldn't write to the specified cache location, try a
             # similar place but inside our home directory instead.
             cache = alternative_cache

.travis.yml
@@ -1,7 +1,6 @@
 sudo: true
 language: python
 
-# Twisted dropped Python 2.6 support, so install a compatible version on 2.6
 install:
  - sudo apt-get install swig
  - pip install --upgrade pip setuptools

doc/conf.py
@@ -16,13 +16,10 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
-import os
-
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-#sys.path.append(os.path.abspath('.'))
+# sys.path.append(os.path.abspath('.'))
 
 # General configuration
 # ---------------------
@@ -41,39 +38,39 @@ html_theme_path = [csp.get_theme_dir()]
 
 # [optional] set some of the options listed above...
 html_theme_options = {
-    "sidebarwidth":     "200px",
-    "max_width":        "900px",
-    "compact_width":    "800px",
-    "minimal_width":    "700px",
+    "sidebarwidth": "200px",
+    "max_width": "900px",
+    "compact_width": "800px",
+    "minimal_width": "700px",
 
     # Style it like Fedora..
-    "bodyfont":         "Cantarell",
+    "bodyfont": "Cantarell",
 
-    "highlightcolor":   "#4FAF4F",
+    "highlightcolor": "#4FAF4F",
 
-    "sidebarbgcolor":   "#FEFEFE",
+    "sidebarbgcolor": "#FEFEFE",
     "sidebartrimcolor": "#FEFEFE",
 
-    "sectionbgcolor":   "#FEFEFE",
+    "sectionbgcolor": "#FEFEFE",
     "sectiontrimcolor": "#FEFEFE",
     "sectiontextcolor": "#444444",
 
-    "relbarbgcolor":    "#FEFEFE",
-    "relbartextcolor":  "#444444",
-    "relbarlinkcolor":  "#444444",
+    "relbarbgcolor": "#FEFEFE",
+    "relbartextcolor": "#444444",
+    "relbarlinkcolor": "#444444",
 
-    "bgcolor":          "#FEFEFE",
-    "textcolor":        "#444444",
-    "linkcolor":        "#060",  # First Green
+    "bgcolor": "#FEFEFE",
+    "textcolor": "#444444",
+    "linkcolor": "#060",  # First Green
 
-    "headtextcolor":    "#444444",
-    "headlinkcolor":    "#444444",
+    "headtextcolor": "#444444",
+    "headlinkcolor": "#444444",
 
-    #"codebgcolor"
-    #"codetextcolor"
-    "codetrimcolor":    "#060",  # First Green
+    # "codebgcolor"
+    # "codetextcolor"
+    "codetrimcolor": "#060",  # First Green
 
-    "footerbgcolor":    "#FEFEFE",
+    "footerbgcolor": "#FEFEFE",
 
 }
 
@@ -89,7 +86,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The encoding of source files.
-#source_encoding = 'utf-8'
+# source_encoding = 'utf-8'
 
 # The master toctree document.
 master_doc = 'index'
@@ -109,16 +106,16 @@ version = '0.1'
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.
-#language = None
+# language = None
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
-#today = ''
+# today = ''
 # Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
+# today_fmt = '%B %d, %Y'
 
 # List of documents that shouldn't be included in the build.
-#unused_docs = []
+# unused_docs = []
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
@@ -126,18 +123,18 @@ exclude_trees = ['.build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
-#default_role = None
+# default_role = None
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
+# add_function_parentheses = True
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
-#add_module_names = True
+# add_module_names = True
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
-#show_authors = False
+# show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -153,19 +150,19 @@ html_style = 'site.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+# html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+# html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+# html_logo = None
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+# html_favicon = None
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -174,11 +171,11 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+# html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-#html_use_smartypants = True
+# html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
@@ -192,27 +189,27 @@ html_sidebars = {
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-#html_additional_pages = {}
+# html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_use_modindex = True
+# html_use_modindex = True
 
 # If false, no index is generated.
-#html_use_index = True
+# html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
-#html_split_index = False
+# html_split_index = False
 
 # If true, the reST sources are included in the HTML build as _sources/<name>.
-#html_copy_source = True
+# html_copy_source = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#html_use_opensearch = ''
+# html_use_opensearch = ''
 
 # If nonempty, this is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = ''
+# html_file_suffix = ''
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'fedmsg'

extras/stress/mass-sub.py
@@ -37,6 +37,7 @@ class ThreadedJob(threading.Thread):
         topic, msg = self.s.recv_multipart()
         sys.stdout.flush()
 
+
 threads = [ThreadedJob() for i in range(N)]
 for thread in threads:
     thread.start()

fedmsg.d/base.py
@@ -17,8 +17,6 @@
 #
 # Authors:  Ralph Bean <rbean@redhat.com>
 
-import os
-
 config = dict(
     # Set this to dev if you're hacking on fedmsg or an app.
     # Set to stg or prod if running in the Fedora Infrastructure
@@ -31,11 +29,11 @@ config = dict(
     ## For the fedmsg-hub and fedmsg-relay. ##
 
     # This is a status dir to keep a record of the last processed message
-    #status_directory=os.getcwd() + "/status",
-    #status_directory='/var/run/fedmsg/status',
+    # status_directory=os.getcwd() + "/status",
+    # status_directory='/var/run/fedmsg/status',
 
     # This is the URL of a datagrepper instance that we can query for backlog.
-    #datagrepper_url="https://apps.fedoraproject.org/datagrepper/raw",
+    # datagrepper_url="https://apps.fedoraproject.org/datagrepper/raw",
 
     # We almost always want the fedmsg-hub to be sending messages with zmq as
     # opposed to amqp or stomp.  You can send with only *one* of the messaging
@@ -46,12 +44,12 @@ config = dict(
 
     # On the other hand, if you wanted to use STOMP *instead* of zeromq, you
     # could do the following...
-    #zmq_enabled=False,
-    #stomp_uri='localhost:59597,localhost:59598',
-    #stomp_user='username',
-    #stomp_pass='password',
-    #stomp_ssl_crt='/path/to/an/optional.crt',
-    #stomp_ssl_key='/path/to/an/optional.key',
+    # zmq_enabled=False,
+    # stomp_uri='localhost:59597,localhost:59598',
+    # stomp_user='username',
+    # stomp_pass='password',
+    # stomp_ssl_crt='/path/to/an/optional.crt',
+    # stomp_ssl_key='/path/to/an/optional.key',
 
     # When subscribing to messages, we want to allow splats ('*') so we tell
     # the hub to not be strict when comparing messages topics to subscription

fedmsg.d/endpoints.py
@@ -26,13 +26,13 @@ config = dict(
         # infrastructure's bus.  Cool, right?  :)
         "fedora-infrastructure": [
             "tcp://hub.fedoraproject.org:9940",
-            #"tcp://stg.fedoraproject.org:9940",
+            # "tcp://stg.fedoraproject.org:9940",
         ],
-        #"debian-infrastructure": [
+        # "debian-infrastructure": [
         #    "tcp://fedmsg.olasd.eu:9940",
-        #],
-        #"anitya-public-relay": [
+        # ],
+        # "anitya-public-relay": [
         #    "tcp://release-monitoring.org:9940",
-        #],
+        # ],
     },
 )

fedmsg.d/ssl.py
@@ -18,7 +18,6 @@
 # Authors:  Ralph Bean <rbean@redhat.com>
 #
 import os
-import socket
 
 SEP = os.path.sep
 here = os.getcwd()
@@ -43,7 +42,7 @@ config = dict(
     certnames={
         # In prod/stg, map hostname to the name of the cert in ssldir.
         # Unfortunately, we can't use socket.getfqdn()
-        #"app01.stg": "app01.stg.phx2.fedoraproject.org",
+        # "app01.stg": "app01.stg.phx2.fedoraproject.org",
     },
 
     # A mapping of fully qualified topics to a list of cert names for which

fedmsg/__init__.py
@@ -21,6 +21,7 @@
 
 import inspect
 import threading
+import functools
 
 import fedmsg.core
 import fedmsg.config
@@ -65,6 +66,7 @@ def API_function(doc=None):
     def api_function(func):
         scrub = inspect.getargspec(func).args
 
+        @functools.wraps(func)
         def _wrapper(*args, **kw):
             if not hasattr(__local, '__context'):
                 config_overrides = kw.copy()
@@ -82,16 +84,14 @@ def API_function(doc=None):
             _wrapper.__doc__ = func.__doc__
         else:
             _wrapper.__doc__ = doc
-
-        _wrapper.__name__ = func.__name__
         return _wrapper
-
     return api_function
 
 
 @API_function(doc=fedmsg.core.FedMsgContext.publish.__doc__)
 def publish(topic=None, msg=None, **kw):
     return __local.__context.publish(topic, msg, **kw)
+
 
 # This is old-school, and deprecated.
 send_message = publish

commit 4c9402ead236124953a8d74d16563f1826695dec

Allow validating unicode signatures.
I just noticed this in the logs of program driven by `fedmsg.tail_messages`.
The signature was unicode, and so this type check failed every time.

This seems to have been inadvertently broken in f0553d285e586dc48f1a244b81f1517551d469ee ?

fedmsg/crypto/x509.py
@@ -113,7 +113,7 @@ def _m2crypto_validate(message, ssldir=None, **config):
     for field in ['signature', 'certificate']:
         if field not in message:
             return fail("No %r field found." % field)
-        if not isinstance(message[field], str):
+        if not isinstance(message[field], basestring):
             return fail("msg[%r] is not a string" % field)
 
     # Peal off the auth datums